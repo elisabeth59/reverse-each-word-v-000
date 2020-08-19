8b8b173d0c16b0b1bbe9cdff22d62a6d249737a4
Elisabeth Narine <elisabethnarine@gmail.com>
2020-08-19 00:53:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,3 @@
-sentence1 = [ "Hello there, and how are you?" ]
-
-def reverse_each_word(sentence1)
-  sentence1.reverse do |sentence1|
-    puts [ "Hello there, and how are you?" ]
-  end
+def reverse_each_word(sentence)
+  sentence.split.collect { |word| word.reverse}.join(" ")
 end
