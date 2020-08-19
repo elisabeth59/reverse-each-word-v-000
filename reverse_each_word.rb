93dd2ef188a98685026e46c8b77d81760b5aebf5
Elisabeth Narine <elisabethnarine@gmail.com>
2020-08-19 00:38:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ sentence1 = [ "Hello there, and how are you?" ]
 
 def reverse_each_word(sentence1)
   sentence1.reverse do |sentence1|
-    puts [sentence1]
+    puts ["olleH ,ereht dna woh era ?uoy"]
   end
 end
